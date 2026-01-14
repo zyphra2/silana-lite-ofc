e9fe4bad847757abad9103fe84a1d04f81077288
Aya <hdiyan122@gmail.com>
2026-01-14 14:52:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 
 // Owner
 global.owner = [
-['212717457920', 'Noureddine', true]
+['212728254498', 'Aya Es-samlaly', true]
 ]
 global.mods = []
 global.prems = []
